f51facb4abe502eefd38e1ca1fd58c53ed984c1e
Gina <ginamantonini@gmail.com>
2018-02-10 13:34:50 -0600

    1, 5, 9 passed testing

---
+++ RomanNumerals.app/NumberConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals.app
+{
+    public class NumberConverter
+    {
+        public string Parse(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return "I";
+                case 5:
+                    return "V";
+                case 9:
+                    return "IX"; 
+            }
+
+            return "another thing";
+        }
+    }
+}
---
+++ RomanNumerals.tests/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RomanNumerals.app;
+
+namespace RomanNumerals.tests
+{
+    [TestFixture]
+    public class RomanNumeralConversionTests
+    {
+        [Test]
+        public void Converting_integer_one_to_a_string()
+        {
+            //Arrange
+            int number = 1;
+            var numberConverter = new NumberConverter();
+
+            //Act
+            var result = numberConverter.Parse(number);
+
+            //Assert
+            Assert.AreEqual("I", result);
+        }
+
+        [Test]
+        public void Converting_integer_five_to_a_string()
+        {
+            //Arrange
+            int number = 5;
+            var numberConverter = new NumberConverter();
+
+            //Act
+            var result = numberConverter.Parse(number);
+
+            //Assert
+            Assert.AreEqual("V", result);
+        }
+
+        [Test]
+        public void Converting_integer_nine_to_a_string()
+        {
+            //Arrange
+            int number = 9;
+            var numberConverter = new NumberConverter();
+
+            //Act
+            var result = numberConverter.Parse(number);
+
+            //Assert
+            Assert.AreEqual("IX", result);
+        }
+    }
+}
